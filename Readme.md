8e5f5931e5e82a02f5fc91fee357d8015c72aafa
zenvar <acatmad@outlook.com>
2024-11-04 04:46:09 +0800

    fix:Readme

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
+![alt text](a-3d-clean-minimalism-design-logo-of-the-eye-of-pr-QtEwgdKATTeM6ckh8hw-SA-MW24cNULRGi27_o9mGYvrg-removebg-preview.png)
+<img src="https://github.com/Pr-QtEwgdKATTeM6ckh8hw-SA-MW24cNULRGi27_o9mGYvrg/blob/main/a-3d-clean-minimalism-design-logo-of-the-eye-of-pr-QtEwgdKATTeM6ckh8hw-SA-MW24cNULRGi27_o9mGYvrg-removebg-preview.png" alt="alt text" stype="width: 50px; height: 50px;">
 # Web3 Blog Scraper
 
 This project is a **TypeScript + Puppeteer** blog scraper designed to extract article links and details from a specified blog. It connects to a **MongoDB database** to store the scraped data.It can **adjust the config selectors** to scrape different parts of the blog. Frontend is built with **Redwood.js**(https://github.com/zenvar/redwoodblog) and connected to the same MongoDB database.
--- main.ts
+++ main.ts
@@ -22,7 +22,7 @@ const config: IBlog = {
     indexPage: '/en-sg/blog/landing',
     articleLinkSelector: 'section div.cds-flex-f1g67tkn.sc-20f7f24c-0.sc-3dcf3304-1.dxlxFg.dZjcVO a',
     nextpageSelector: '#__next > div > div > div > div > div.cds-flex-f1g67tkn.sc-20f7f24c-0.jxwgNN > div > div > div > div > button',
-    detailsSelector: '#__next > div > div > div > div > div.cds-flex-f1g67tkn.sc-20f7f24c-0.jxwgNN > div > div > div.cds-flex-f1g67tkn.sc-20f7f24c-0.jqnGZy > section',
+    detailsSelector: '#__next > div > div > div > div > div.cds-flex-f1g67tkn.sc-20f7f24c-0.jxwgNN > div > div > div.cds-flex-f1g67tkn.sc-20f7f24c-0.jqnGZy section',
     timeselector: '#article_introduction > header > div > div > div:nth-child(1) > div > div.cds-flex-f1g67tkn.sc-20f7f24c-0.ioIUsw > div.cds-flex-f1g67tkn.sc-20f7f24c-0.hjdKBS > p:nth-child(2)',
     headerselector: 'h1',
     _id: '001'
